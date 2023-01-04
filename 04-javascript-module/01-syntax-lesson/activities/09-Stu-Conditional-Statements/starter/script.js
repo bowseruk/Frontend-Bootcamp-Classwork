1353d6c9ec7e265bb7172a12990342c8827b0c82
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-01-04 15:28:27 +0000

    Completed day 1 week 4

--- 04-javascript-module/01-syntax-lesson/activities/09-Stu-Conditional-Statements/starter/script.js
+++ 04-javascript-module/01-syntax-lesson/activities/09-Stu-Conditional-Statements/starter/script.js
@@ -1,6 +1,26 @@
 // Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var expression1 = true;
+var expression2 = true;
 
 // Write Your JavaScript Code Here
+console.log((expression1 ? "True ✅" : "False ❌") + " " + (expression2 ? "True ✅" : "False ❌"));
+
+var expression1 = true;
+var expression2 = false;
+
+// Write Your JavaScript Code Here
+console.log((expression1 ? "True ✅" : "False ❌") + " " + (expression2 ? "True ✅" : "False ❌"));
+
+var expression1 = false;
+var expression2 = true;
+
+// Write Your JavaScript Code Here
+console.log((expression1 ? "True ✅" : "False ❌") + " " + (expression2 ? "True ✅" : "False ❌"));
+
+var expression1 = false;
+var expression2 = false;
+
+// Write Your JavaScript Code Here
+console.log((expression1 ? "True ✅" : "False ❌") + " " + (expression2 ? "True ✅" : "False ❌"));
+
