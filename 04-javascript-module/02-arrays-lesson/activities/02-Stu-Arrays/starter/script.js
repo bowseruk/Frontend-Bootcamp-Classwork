ce9dfb122e6fbd932a43ada9f6624ea78b602901
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-01-06 23:03:22 +0000

    Completed week 4

--- 04-javascript-module/02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
+++ 04-javascript-module/02-arrays-lesson/activities/02-Stu-Arrays/starter/script.js
@@ -1,1 +1,19 @@
 // WRITE YOUR CODE HERE
+// Array of names
+var student = "Tom"
+var students = [student, "Dick", "Harry"]
+// Length of array
+console.log(students.length)
+// For loop to go through each item in array
+for (let i = 0; i < students.length; i++) {
+    console.log("Welcome to the class " + students[i]);
+}
+// console.log("Welcome to the class " + students[0]);
+// console.log("Welcome to the class " + students[1]);
+// console.log("Welcome to the class " + students[2]);
+// Rename the first name
+students[0] = "Freddie"
+// Check the first name has changed
+if (students[0] !== student) {
+    console.log(students[0] + " is in class")
+}
