7ede4c44891bf2aaa2fc2829bcdc4b2fabd6e4f6
Anthony Silva <1xajsilva@gmail.com>
2023-02-02 11:58:00 -0600

    remove solution

--- 08-server-apis-module/02-api-practice-lesson/activities/11-BandsInTownApp/starter/script.js
+++ 08-server-apis-module/02-api-practice-lesson/activities/11-BandsInTownApp/starter/script.js
@@ -1,16 +1,45 @@
 function searchBandsInTown(artist) {
   // Add code to query the bands in town api searching for the artist received as an argument to this function
-  // Using jQuery, append the following to the #artist-div :
-  // The artist's name
-  // The artists thumbnail image
-  // The number of fans tracking this artist
-  // The number of upcoming events for this artist
-  // A link to the bandsintown url for this artist
+  var queryURL =
+    "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // The artist's name
+    var artistName = $("<h1>").text(response.name);
+
+    // The artists thumbnail image
+    var artistImage = $("<img>").attr("src", response.thumb_url);
+
+    // The number of fans tracking this artist
+    var trackerCount = $("<h2>").text(
+      response.tracker_count + " fans tracking this artist"
+    );
+
+    // The number of upcoming events for this artist
+    var upcomingEvents = $("<h2>").text(
+      response.upcoming_event_count + " upcoming events"
+    );
+
+    // A link to the bandsintown url for this artist
+    var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
+
+    // Using jQuery, append the following to the #artist-div :
+    $("#artist-div").append(
+      artistName,
+      artistImage,
+      trackerCount,
+      upcomingEvents,
+      goToArtist
+    );
+  });
   // Note: Append actual HTML elements, not just text
 }
 
 // Event handler for user clicking the select-artist button
-$("#select-artist").on("click", function(event) {
+$("#select-artist").on("click", function (event) {
   // Preventing the button from trying to submit the form
   event.preventDefault();
   // Storing the artist name
