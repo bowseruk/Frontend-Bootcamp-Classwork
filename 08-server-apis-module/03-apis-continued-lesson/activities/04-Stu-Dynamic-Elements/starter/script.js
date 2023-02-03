d29ca775863bfd9689cde242ba4a2b4781f9054e
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-02-03 15:57:33 +0000

    Finish of day 3 week 8

--- 08-server-apis-module/03-apis-continued-lesson/activities/04-Stu-Dynamic-Elements/starter/script.js
+++ 08-server-apis-module/03-apis-continued-lesson/activities/04-Stu-Dynamic-Elements/starter/script.js
@@ -1,4 +1,4 @@
-$("button").on("click", function() {
+$("button").on("click", function () {
   var animal = $(this).attr("data-animal");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     animal + "&api_key=NJRTjUwB961UtondT1ugXEQlYQaT4H9k&limit=10";
@@ -6,32 +6,30 @@ $("button").on("click", function() {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
     // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
-
     console.log(response);
-
     // Step 2: since the image information is inside of the data key,
     // make a variable named results and set it equal to response.data
-    
+
     // =============== put step 2 in between these dashes ==================
-let results = response.data
+    let results = response.data;
     // ========================
 
     for (var i = 0; i < results.length; i++) {
 
-    // Step 3: uncomment the for loop above and the closing curly bracket below.
-    // Make a div with jQuery and store it in a variable named animalDiv.
-    // Make a paragraph tag with jQuery and store it in a variable named p.
-    // Set the inner text of the paragraph to the rating of the image in results[i].
-    // Make an image tag with jQuery and store it in a variable named animalImage.
-    // Set the image's src to results[i]'s fixed_height.url.
-    // Append the p variable to the animalDiv variable.
-    // Append the animalImage variable to the animalDiv variable.
-    // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
+      // Step 3: uncomment the for loop above and the closing curly bracket below.
+      // Make a div with jQuery and store it in a variable named animalDiv.
+      // Make a paragraph tag with jQuery and store it in a variable named p.
+      // Set the inner text of the paragraph to the rating of the image in results[i].
+      // Make an image tag with jQuery and store it in a variable named animalImage.
+      // Set the image's src to results[i]'s fixed_height.url.
+      // Append the p variable to the animalDiv variable.
+      // Append the animalImage variable to the animalDiv variable.
+      // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
 
-    // ============= put step 3 in between these dashes ======================
+      // ============= put step 3 in between these dashes ======================
       let animalDiv = $('<div>');
       let p = $('<p>');
       p.text(results[i].rating);
@@ -41,8 +39,7 @@ let results = response.data
       animalDiv.append(animalImage);
       $('#gifs-appear-here').prepend(animalDiv);
 
-
-    // ==================================
+      // ==================================
     }
 
   });
