5eff72e7251a41a39d78f513f1002b6e5ec3f012
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-03-09 14:57:50 +0000

    fetching data in react

---
+++ 13-react-module-modified/03-react-router-lesson/activities/fetching-data-2/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PhotoFetcher from "./components/PhotoFetcher";
+import PhotoFetcherFail from "./components/PhotoFetcherFail";
+
+function App() {
+  return <PhotoFetcher />;
+}
+
+export default App;
